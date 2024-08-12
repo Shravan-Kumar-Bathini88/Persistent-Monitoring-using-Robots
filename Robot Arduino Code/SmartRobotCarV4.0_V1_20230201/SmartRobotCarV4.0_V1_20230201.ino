4edbf6deb476f2c40ddb39c504620cc8616f689f
Shravan-Kumar-Bathini88 <156976168+Shravan-Kumar-Bathini88@users.noreply.github.com>
2024-08-12 12:12:12 -0400

    Add files via upload

---
+++ Robot Arduino Code/SmartRobotCarV4.0_V1_20230201/SmartRobotCarV4.0_V1_20230201.ino
@@ -0,0 +1,72 @@
+/*
+ * @Author: ELEGOO
+ * @Date: 2019-10-22 11:59:09
+ * @LastEditTime: 2020-12-18 14:14:35
+ * @LastEditors: Changhua
+ * @Description: Smart Robot Car V4.0
+ * @FilePath: 
+ */
+#include <avr/wdt.h>
+#include "ApplicationFunctionSet_xxx0.h"
+#define TRIG_PIN 13      // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN 12      // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 500 
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Application_FunctionSet.ApplicationFunctionSet_Init();
+  wdt_enable(WDTO_2S);
+  //Serial.begin(9600);
+}
+
+void loop()
+{
+  //put your main code here, to run repeatedly :
+  wdt_reset();
+  Application_FunctionSet.ApplicationFunctionSet_SensorDataUpdate();
+  Application_FunctionSet.ApplicationFunctionSet_KeyCommand();
+  Application_FunctionSet.ApplicationFunctionSet_RGB();
+  Application_FunctionSet.ApplicationFunctionSet_Follow();
+  Application_FunctionSet.ApplicationFunctionSet_Obstacle();
+  Application_FunctionSet.ApplicationFunctionSet_Tracking();
+  Application_FunctionSet.ApplicationFunctionSet_Rocker();
+  Application_FunctionSet.ApplicationFunctionSet_Standby();
+  Application_FunctionSet.ApplicationFunctionSet_IRrecv();
+  Application_FunctionSet.ApplicationFunctionSet_SerialPortDataAnalysis();
+
+  Application_FunctionSet.CMD_ServoControl_xxx0();
+  Application_FunctionSet.CMD_MotorControl_xxx0();
+  Application_FunctionSet.CMD_CarControlTimeLimit_xxx0();
+  Application_FunctionSet.CMD_CarControlNoTimeLimit_xxx0();
+  Application_FunctionSet.CMD_MotorControlSpeed_xxx0();
+  Application_FunctionSet.CMD_LightingControlTimeLimit_xxx0();
+  Application_FunctionSet.CMD_LightingControlNoTimeLimit_xxx0();
+  Application_FunctionSet.CMD_ClearAllFunctions_xxx0();
+ unsigned int ultrasonic_reading = ultrasonic();
+ Serial.println(ultrasonic_reading);
+}
+
+int ultrasonic()
+{
+  unsigned int tempda_x = 0;
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  tempda_x = ((unsigned int)pulseIn(ECHO_PIN, HIGH) / 58);
+  // ULTRASONIC_Get = tempda_x;
+  return tempda_x;
+
+  //if (tempda_x > 150)
+  //{
+  //  *ULTRASONIC_Get = 150;
+  //}
+  //else
+  //{
+  //  *ULTRASONIC_Get = tempda_x;
+  //}
+  // sonar.ping() / US_ROUNDTRIP_CM; // Send ping, get ping time in microseconds (uS).
+}
